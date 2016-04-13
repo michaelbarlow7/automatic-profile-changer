ba3b08099381a6b0c0b72456b563e22f30a3fcef
Michael Barlow <michaelbarlow7@gmail.com>
2016-04-13 20:33:39 +1000

    Added profile-manager.jar containing classes used by Cyangogenmod to CRUD profiles, with some test code

--- app/src/main/java/com/mbarlow/automaticprofilechanger/MainActivity.kt
+++ app/src/main/java/com/mbarlow/automaticprofilechanger/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.mbarlow.automaticprofilechanger
 
+import android.app.ProfileManager
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+
         fab.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+
+        ringerModeNormal.setOnClickListener { view ->
+            var profileManager = ProfileManager.getService();
+            profileManager.setActiveProfileByName("Default");
+            Log.d("APC", "Ringer mode default");
+        }
+
+        ringerModeSilent.setOnClickListener { view ->
+            var profileManager = ProfileManager.getService();
+            profileManager.setActiveProfileByName("Silent");
+            Log.d("APC", "Ringer mode silent");
+        }
+
+        ringerModeVibrate.setOnClickListener { view ->
+            var profileManager = ProfileManager.getService();
+            profileManager.setActiveProfileByName("Vibrate");
+            Log.d("APC", "Ringer mode vibrate");
+        }
     }
 }
