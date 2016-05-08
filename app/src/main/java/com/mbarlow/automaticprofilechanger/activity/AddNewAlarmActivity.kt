10094f89beae63311414401ca565986d0525c71b
Michael Barlow <michaelbarlow7@gmail.com>
2016-05-08 12:38:59 +1000

    Did layout for edit/add alarm screen

--- app/src/main/java/com/mbarlow/automaticprofilechanger/activity/AddNewAlarmActivity.kt
+++ app/src/main/java/com/mbarlow/automaticprofilechanger/activity/AddNewAlarmActivity.kt
@@ -3,6 +3,7 @@ package com.mbarlow.automaticprofilechanger.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.mbarlow.automaticprofilechanger.R
+import kotlinx.android.synthetic.main.activity_add_alarm.*
 
 /**
  * Created by michael on 8/05/16.
@@ -12,5 +13,6 @@ class AddNewAlarmActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_alarm)
+        setSupportActionBar(toolbar)
     }
 }
